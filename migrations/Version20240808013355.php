abb4070486237b154272cfd28654a8e6ed5a0a5b
Fil <filjoseph22@gmail.com>
2024-08-08 10:09:34 +0800

    added more test cases

--- migrations/Version20240808013355.php
+++ migrations/Version20240808013355.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240807153132 extends AbstractMigration
+final class Version20240808013355 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,7 @@ final class Version20240807153132 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE customers_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE customers (id INT NOT NULL, uuid VARCHAR(255) DEFAULT NULL, title VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, gender VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, dob DATE DEFAULT NULL, registered_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, phone VARCHAR(255) DEFAULT NULL, cell VARCHAR(255) DEFAULT NULL, nat VARCHAR(255) DEFAULT NULL, picture_large VARCHAR(255) DEFAULT NULL, picture_medium VARCHAR(255) DEFAULT NULL, picture_thumbnail VARCHAR(255) DEFAULT NULL, roles JSON DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE customers (id INT NOT NULL, uuid VARCHAR(255) DEFAULT NULL, title VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, gender VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, dob DATE DEFAULT NULL, registered_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, phone VARCHAR(255) DEFAULT NULL, cell VARCHAR(255) DEFAULT NULL, nat VARCHAR(255) DEFAULT NULL, picture_large VARCHAR(255) DEFAULT NULL, picture_medium VARCHAR(255) DEFAULT NULL, picture_thumbnail VARCHAR(255) DEFAULT NULL, roles JSON DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX unique_email ON customers (email)');
     }
 
--- tests/Service/DataImporterTest.php
+++ tests/Service/DataImporterTest.php
@@ -32,74 +32,22 @@ class DataImporterTest extends TestCase
         $this->lockFactory = new LockFactory($store);
 
         $mockResponse = new MockResponse(json_encode([
-            'results' => [
-                [
-                    "gender" => "female",
-                    "name" => [
-                        "title" => "Miss",
-                        "first" => "Amy",
-                        "last" => "Fortin"
-                    ],
-                    "location" => [
-                        "street" => [
-                            "number" => 3772,
-                            "name" => "Arctic Way"
-                        ],
-                        "city" => "Shelbourne",
-                        "state" => "Prince Edward Island",
-                        "country" => "Canada",
-                        "postcode" => "B4G 7F4",
-                        "coordinates" => [
-                            "latitude" => "-64.4509",
-                            "longitude" => "28.6265"
-                        ],
-                        "timezone" => [
-                            "offset" => "+3:00",
-                            "description" => "Baghdad, Riyadh, Moscow, St. Petersburg"
-                        ]
-                    ],
-                    "email" => "amy.fortin@example.com",
-                    "login" => [
-                        "uuid" => "140862bb-eba5-4a51-a87a-a9bd9bb4a765",
-                        "username" => "purpleostrich632",
-                        "password" => "iverson3",
-                        "salt" => "RMvNuejH",
-                        "md5" => "c3309d1538f169b85604f905742e3458",
-                        "sha1" => "a78b15144a2a07abfad3a08aa58d6bd04b54980c",
-                        "sha256" => "819b5cade8febac7dd143140554c3d7c77a4d2af802a70d82b311d102b31e3a3"
-                    ],
-                    "dob" => [
-                        "date" => "1985-08-08T00:09:08.148Z",
-                        "age" => 38
-                    ],
-                    "registered" => [
-                        "date" => "2019-01-18T08:51:46.332Z",
-                        "age" => 5
-                    ],
-                    "phone" => "S72 Q43-3165",
-                    "cell" => "E18 B11-3216",
-                    "id" => [
-                        "name" => "SIN",
-                        "value" => "926242322"
-                    ],
-                    "picture" => [
-                        "large" => "https://example.com/api/portraits/women/59.jpg",
-                        "medium" => "https://example.com/api/portraits/med/women/59.jpg",
-                        "thumbnail" => "https://example.com/api/portraits/thumb/women/59.jpg"
-                    ],
-                    "nat" => "AU"
-                ]
-            ]
+            'results' => $this->getResults()
         ]));
 
         $this->httpClient = new MockHttpClient($mockResponse);
 
+        $this->dataImporter = $this->createDataImporter();
+    }
+
+    private function createDataImporter(): DataImporter
+    {
         // Set environment variables directly in the test
         $apiUrl = getenv('API_URL') ?: 'http://example.com/api';
         $defaultNationality = getenv('DEFAULT_NATIONALITY') ?: 'AU';
-        $defaultResults = (int) getenv('DEFAULT_RESULTS') ?: 10;
+        $defaultResults = (int) getenv('DEFAULT_RESULTS') ?: 1;
 
-        $this->dataImporter = new DataImporter(
+        return new DataImporter(
             $this->entityManager,
             $apiUrl,
             $defaultNationality,
@@ -110,7 +58,69 @@ class DataImporterTest extends TestCase
         );
     }
 
-    public function testImportCustomers()
+    private function getResults(): array
+    {
+        return [
+            [
+                "gender" => "female",
+                "name" => [
+                    "title" => "Miss",
+                    "first" => "Amy",
+                    "last" => "Fortin"
+                ],
+                "location" => [
+                    "street" => [
+                        "number" => 3772,
+                        "name" => "Arctic Way"
+                    ],
+                    "city" => "Shelbourne",
+                    "state" => "Prince Edward Island",
+                    "country" => "Australia",
+                    "postcode" => "B4G 7F4",
+                    "coordinates" => [
+                        "latitude" => "-64.4509",
+                        "longitude" => "28.6265"
+                    ],
+                    "timezone" => [
+                        "offset" => "+3:00",
+                        "description" => "Baghdad, Riyadh, Moscow, St. Petersburg"
+                    ]
+                ],
+                "email" => "amy.fortin@example.com",
+                "login" => [
+                    "uuid" => "140862bb-eba5-4a51-a87a-a9bd9bb4a765",
+                    "username" => "purpleostrich632",
+                    "password" => "iverson3",
+                    "salt" => "RMvNuejH",
+                    "md5" => "c3309d1538f169b85604f905742e3458",
+                    "sha1" => "a78b15144a2a07abfad3a08aa58d6bd04b54980c",
+                    "sha256" => "819b5cade8febac7dd143140554c3d7c77a4d2af802a70d82b311d102b31e3a3"
+                ],
+                "dob" => [
+                    "date" => "1985-08-08T00:09:08.148Z",
+                    "age" => 38
+                ],
+                "registered" => [
+                    "date" => "2019-01-18T08:51:46.332Z",
+                    "age" => 5
+                ],
+                "phone" => "S72 Q43-3165",
+                "cell" => "E18 B11-3216",
+                "id" => [
+                    "name" => "SIN",
+                    "value" => "926242322"
+                ],
+                "picture" => [
+                    "large" => "https://example.com/api/portraits/women/59.jpg",
+                    "medium" => "https://example.com/api/portraits/med/women/59.jpg",
+                    "thumbnail" => "https://example.com/api/portraits/thumb/women/59.jpg"
+                ],
+                "nat" => "AU"
+            ]
+        ];
+    }
+
+    public function testImportCustomersSuccessfully()
     {
         $this->entityManager->expects($this->atLeastOnce())
             ->method('persist')
@@ -119,11 +129,102 @@ class DataImporterTest extends TestCase
                 $this->assertEquals('amy.fortin@example.com', $customer->getEmail());
             });
 
-        $this->entityManager->expects($this->atLeastOnce())
-            ->method('flush');
+        $this->entityManager->expects($this->atLeastOnce())->method('flush');
 
         $result = $this->dataImporter->importCustomers();
 
         $this->assertEquals('Successfully imported 1 customers.', $result);
+    }
+
+    public function testImportCustomersNoResults()
+    {
+        $mockResponse = new MockResponse(json_encode([
+            'results' => []
+        ]));
+
+        $this->httpClient = new MockHttpClient($mockResponse);
+
+        $this->dataImporter = $this->createDataImporter();
+
+        $this->entityManager->expects($this->never())->method('persist');
+
+        $this->entityManager->expects($this->never())->method('flush');
+
+        $result = $this->dataImporter->importCustomers();
+
+        $this->assertEquals('Successfully imported 0 customers.', $result);
+    }
+
+    public function testImportCustomersInvalidData()
+    {
+        $mockResponse = new MockResponse(json_encode([
+            'results' => [
+                [ "email" => "invalid-email" ]
+            ]
+        ]));
+
+        $this->httpClient = new MockHttpClient($mockResponse);
+
+        $this->dataImporter = $this->createDataImporter();
+
+        $this->entityManager->expects($this->never())
+            ->method('persist');
+
+        $this->entityManager->expects($this->never())
+            ->method('flush');
+
+        $result = $this->dataImporter->importCustomers();
+
+        $this->assertEquals('Successfully imported 0 customers.', $result);
+    }
+
+    public function testImportCustomersApiFailure()
+    {
+        $mockResponse = new MockResponse('', ['http_code' => 500]);
+
+        $this->httpClient = new MockHttpClient($mockResponse);
+
+        $this->dataImporter = $this->createDataImporter();
+
+        $this->entityManager->expects($this->never())->method('persist');
+
+        $this->entityManager->expects($this->never())->method('flush');
+
+        $result = $this->dataImporter->importCustomers();
+
+        $this->assertEquals('Failed to fetch data from API.', $result);
+    }
+
+    public function testImportCustomersLockFailure()
+    {
+        // $lock = $this->createMock(\Symfony\Component\Lock\LockInterface::class);
+        $lock = $this->createMock(\Symfony\Component\Lock\SharedLockInterface::class);
+        $lock->expects($this->once())->method('acquire')->willReturn(false);
+
+        $lockFactory = $this->createMock(LockFactory::class);
+        $lockFactory->expects($this->once())->method('createLock')->willReturn($lock);
+
+        $this->dataImporter->setLockFactory($lockFactory);
+
+        $result = $this->dataImporter->importCustomers();
+
+        $this->assertEquals('Another import process is already running.', $result);
+    }
+
+    public function testImportCustomersDataFetchFailure()
+    {
+        $mockResponse = new MockResponse('', ['http_code' => 500]);
+
+        $this->httpClient = new MockHttpClient($mockResponse);
+
+        $this->dataImporter = $this->createDataImporter();
+
+        $this->entityManager->expects($this->never())->method('persist');
+
+        $this->entityManager->expects($this->never())->method('flush');
+
+        $result = $this->dataImporter->importCustomers();
+
+        $this->assertEquals('Failed to fetch data from API.', $result);
     }
 }
